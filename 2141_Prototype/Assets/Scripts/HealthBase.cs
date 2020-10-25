28525a5a7e3cf51b9464a5a3774bfe1a3ae49f78
Daniel Butcher <1020078student.sae.edu.au>
2020-10-25 14:44:22 +1100

    Project Upload

---
+++ 2141_Prototype/Assets/Scripts/HealthBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBase : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+    public bool isDead;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        // health = health - damage
+        Death();
+    }
+
+    public void Death()
+    {
+        if (health<maxHealth)
+        {
+            isDead = true;
+        }
+    }
+}
---
+++ 2141_Prototype/Assets/Scripts/scroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class scroll : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+}
