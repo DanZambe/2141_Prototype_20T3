6343fafaeeeb556f7172b75cd749eafaeb2e10c8
Assassin53D <Danielphill1998@gmail.com>
2020-11-06 14:30:35 +1100

    2 new enemies

---
+++ 2141_Prototype/Assets/Scripts/PlayerMirrorShooting.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMirrorShooting : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public Transform leftGun;
+    public Transform rightGun;
+    public float shootTimer;
+
+    private Rigidbody2D rb;
+	private float dirX, dirY, moveSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = 5f;
+        StartCoroutine(Shooting());
+    }
+
+	public void Update()
+	{
+        dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
+       // dirY = CrossPlatformInputManager.GetAxis("Vertical") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX, dirY);
+    }
+
+
+    IEnumerator Shooting()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(shootTimer);
+            shoot();
+        }
+    }
+
+    public void shoot()
+    {
+        Instantiate(bullet, rightGun.position, rightGun.rotation);
+        Instantiate(bullet, leftGun.position, leftGun.rotation);
+
+    }
+}
---
+++ 2141_Prototype/Assets/Scripts/StealthBomber.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StealthBomber : MonoBehaviour
+{
+
+    public GameObject StealthBommberOBJ;
+    public Transform Spawn1;
+    public Transform Spawn2;
+	public Transform Spawn3;
+	public Transform Spawn4;
+	public Transform Spawn5;
+	public float shootTimer;
+
+	public void Start()
+	{
+		shoot();
+	}
+
+	//IEnumerator Shooting()
+	//{
+	//    while (true)
+	//    {
+	//        yield return new WaitForSeconds(shootTimer);
+	//        shoot();
+	//    }
+	//}
+
+	public void shoot()
+    {
+        Instantiate(StealthBommberOBJ, Spawn1.position, Spawn1.rotation);
+		Instantiate(StealthBommberOBJ, Spawn2.position, Spawn2.rotation);
+		Instantiate(StealthBommberOBJ, Spawn3.position, Spawn3.rotation);
+		Instantiate(StealthBommberOBJ, Spawn4.position, Spawn4.rotation);
+		Instantiate(StealthBommberOBJ, Spawn5.position, Spawn5.rotation);
+
+	}
+}
