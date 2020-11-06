be8c2fd63d67a0841e4a3316a35cf8d70d5c8fe7
Daniel Butcher <1020078student.sae.edu.au>
2020-11-06 14:52:49 +1100

    Scrolling BG

---
+++ 2141_Prototype/Assets/RepeatBg.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class RepeatBg : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    private Rigidbody2D rb;
+
+    private float width;
+
+    private float speed = -1f;
+
+    
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+
+        width = boxCollider.size.y;
+        rb.velocity = new Vector2(0, speed);
+    }
+
+   
+    void Update()
+    {
+        if (transform.position.y < -width)
+        {
+            Reposition();
+        }
+    }
+    private void Reposition()
+    {
+        Vector2 vector = new Vector2(0, width * 2f);
+        transform.position = (Vector2)transform.position + vector;
+    }
+}
